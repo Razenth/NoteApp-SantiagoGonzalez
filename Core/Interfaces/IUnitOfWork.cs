43aca9ed6bd433159c9dd5c33b189337c807e1c8
Santi <santiago.gonzalezagudelo@gmail.com>
2023-10-17 16:15:51 -0500

    feat: :sparkles: Agregando todas las interfaces entro de IUnitOfWork

---
+++ Core/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IAuditoria Auditorias { get; }
+        IBlockChain BlockChains {get;}
+        IEstadoNotificacion EstadoNotificaciones {get;}
+        IFormato Formatos {get;}
+        IGenericoVsSubmodulo GenericosVsSubmodulos {get;}
+        IHiloRepuestaNotificacion HiloRepuestaNotificaciones {get;}
+        IMaestroVsSubmodulo MaestrosVsSubmodulos {get;}
+        IModuloMaestro ModulosMaestros {get;}
+        IModuloNotificacion ModulosNotificaciones {get;}
+        IPermisoGenerico PermisosGenericos {get;}
+        IRadicado Radicados {get;}
+        IRol Roles {get;}
+        IRolVsMaestro RolesVsMaestro {get;}
+        ISubmodulo Submodulos {get;}
+        ITipoNotificacion TipoNotificaciones {get;}
+        ITipoRequerimiento TipoRequerimientos {get;}
+    }
+}
