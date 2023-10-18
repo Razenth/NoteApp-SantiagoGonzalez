4ca469b0800ecdd0c8017466df7069f313efcc0a
Santi <santiago.gonzalezagudelo@gmail.com>
2023-10-17 23:40:57 -0500

    feat: :sparkles: Realizamos los mapeos de las Entidades principales a Dtos

--- ApiNoteApp/Profiles/MappingProfiles.cs
+++ ApiNoteApp/Profiles/MappingProfiles.cs
@@ -2,10 +2,29 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ApiNoteApp.Dtos;
 using AutoMapper;
+using Core.Entities;
 
 namespace ApiNoteApp.Profiles;
 public class MappingProfiles : Profile
 {
-
+    public MappingProfiles(){
+        CreateMap<Auditoria,AuditoriaDto>().ReverseMap();
+        CreateMap<BlockChain,BlockChainDto>().ReverseMap();
+        CreateMap<EstadoNotificacion, EstadoNotificacionDto>().ReverseMap();
+        CreateMap<Formato,FormatoDto>().ReverseMap();
+        CreateMap<GenericoVsSubmodulo, GenericoVsSubmoduloDto>().ReverseMap();
+        CreateMap<HiloRespuestaNotificacion,HiloRespuestaNotificacionDto>().ReverseMap();
+        CreateMap<MaestroVsSubmodulo,MaestroVsSubmoduloDto>().ReverseMap();
+        CreateMap<ModuloMaestro,ModuloMaestroDto>().ReverseMap();
+        CreateMap<ModuloNotificacion, ModuloNotificacionDto>().ReverseMap();
+        CreateMap<PermisoGenerico, PermisoGenericoDto>().ReverseMap();
+        CreateMap<Radicado, RadicadoDto>().ReverseMap();
+        CreateMap<Rol,RolDto>().ReverseMap();
+        CreateMap<RolVsMaestro,RolVsMaestroDto>().ReverseMap();
+        CreateMap<Submodulo, SubmoduloDto>().ReverseMap();
+        CreateMap<TipoNotificacion, TipoNotificacionDto>().ReverseMap();
+        CreateMap<TipoRequerimiento, TipoRequerimientoDto>().ReverseMap();
+    }
 }
