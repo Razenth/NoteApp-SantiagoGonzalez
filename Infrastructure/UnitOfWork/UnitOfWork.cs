11e1ff325dd8a1bcc46525d7dff88e6ac3bc4b0a
Santi <santiago.gonzalezagudelo@gmail.com>
2023-10-17 19:33:58 -0500

    feat: :sparkles: Agregando las entidades interfaz a UnitOfWork
    Para que sólo se cree una instancia y ahorre memoria, tenemos que usar tanto los repositorios como las interfaces

--- Infrastructure/UnitOfWork/UnitOfWork.cs
+++ Infrastructure/UnitOfWork/UnitOfWork.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Core.Interfaces;
 using Infrastructure.Data;
+using Infrastructure.Repositories;
 
 namespace Infrastructure.UnitOfWork;
 
@@ -14,40 +15,202 @@ public class UnitOfWork : IUnitOfWork, IDisposable
     {
         _context = context;
     }
-    public IAuditoria Auditorias => throw new NotImplementedException();
-
-    public IBlockChain BlockChains => throw new NotImplementedException();
-
-    public IEstadoNotificacion EstadoNotificaciones => throw new NotImplementedException();
-
-    public IFormato Formatos => throw new NotImplementedException();
-
-    public IGenericoVsSubmodulo GenericosVsSubmodulos => throw new NotImplementedException();
-
-    public IHiloRepuestaNotificacion HiloRepuestaNotificaciones => throw new NotImplementedException();
-
-    public IMaestroVsSubmodulo MaestrosVsSubmodulos => throw new NotImplementedException();
-
-    public IModuloMaestro ModulosMaestros => throw new NotImplementedException();
-
-    public IModuloNotificacion ModulosNotificaciones => throw new NotImplementedException();
-
-    public IPermisoGenerico PermisosGenericos => throw new NotImplementedException();
-
-    public IRadicado Radicados => throw new NotImplementedException();
-
-    public IRol Roles => throw new NotImplementedException();
-
-    public IRolVsMaestro RolesVsMaestro => throw new NotImplementedException();
-
-    public ISubmodulo Submodulos => throw new NotImplementedException();
-
-    public ITipoNotificacion TipoNotificaciones => throw new NotImplementedException();
-
-    public ITipoRequerimiento TipoRequerimientos => throw new NotImplementedException();
-
+    private AuditoriaRepository _auditoria;
+    public IAuditoria Auditorias 
+    {
+        get
+        {
+            if(_auditoria == null)
+            {
+                _auditoria = new AuditoriaRepository(_context);
+            }
+            return _auditoria;
+        }
+    }
+    private BlockChainRepository _blockChain;
+    public IBlockChain BlockChains
+    {
+        get
+        {
+            if(_blockChain == null)
+            {
+                _blockChain = new BlockChainRepository(_context);
+            }
+            return _blockChain;
+        }
+    }
+    private EstadoNotificacionRepository _estadoNotifi;
+    public IEstadoNotificacion EstadoNotificaciones
+    {
+        get
+        {
+            if(_estadoNotifi == null)
+            {
+                _estadoNotifi = new EstadoNotificacionRepository(_context);
+            }
+            return _estadoNotifi;
+        }
+    }
+    private FormatoRepository _formato;
+    public IFormato Formatos 
+    {
+        get
+        {
+            if(_formato == null)
+            {
+                _formato = new FormatoRepository(_context);
+            }
+            return _formato;
+        }
+    }
+    private GenericoVsSubmoduloRepository _genericoVsModulo;
+    public IGenericoVsSubmodulo GenericosVsSubmodulos 
+    {
+        get
+        {
+            if(_genericoVsModulo == null)
+            {
+                _genericoVsModulo = new GenericoVsSubmoduloRepository(_context);
+            }
+            return _genericoVsModulo;
+        }
+    }
+    private HiloRespuestaNotificacionRepository _hiloNotificacion;
+    public IHiloRepuestaNotificacion HiloRepuestaNotificaciones 
+    {
+        get
+        {
+            if(_hiloNotificacion == null)
+            {
+                _hiloNotificacion = new HiloRespuestaNotificacionRepository(_context);
+            }
+            return _hiloNotificacion;
+        }
+    }
+    private MaestroVsSubmoduloRepository _maestroVsSubmodulo;
+    public IMaestroVsSubmodulo MaestrosVsSubmodulos
+    {
+        get
+        {
+            if(_maestroVsSubmodulo == null)
+            {
+                _maestroVsSubmodulo = new MaestroVsSubmoduloRepository(_context);
+            }
+            return _maestroVsSubmodulo;
+        }
+    }
+    private ModuloMaestroRepository _moduloMaestro;
+    public IModuloMaestro ModulosMaestros
+    {
+        get
+        {
+            if(_moduloMaestro == null)
+            {
+                _moduloMaestro = new ModuloMaestroRepository(_context);
+            }
+            return _moduloMaestro;
+        }
+    }
+    private ModuloNotificacionRepository _moduloNotificacion;
+    public IModuloNotificacion ModulosNotificaciones
+    {
+        get
+        {
+            if(_moduloNotificacion == null)
+            {
+                _moduloNotificacion = new ModuloNotificacionRepository(_context);
+            }
+            return _moduloNotificacion;
+        }
+    }
+    private PermisoGenericoRepository _permisoGenerico;
+    public IPermisoGenerico PermisosGenericos
+    {
+        get
+        {
+            if(_permisoGenerico == null)
+            {
+                _permisoGenerico = new PermisoGenericoRepository(_context);
+            }
+            return _permisoGenerico;
+        }
+    }
+    private RadicadoRepository _radicado;
+    public IRadicado Radicados
+    {
+        get
+        {
+            if (_radicado == null)
+            {
+                _radicado = new RadicadoRepository(_context);
+            } 
+            return _radicado;
+        }
+    }
+    private RolRepository _rol;
+    public IRol Roles{
+        get
+        {
+            if(_rol == null)
+            {
+                _rol = new RolRepository(_context);
+            }
+            return _rol;
+        }
+    }
+    private RolVsMaestroRepository _rolVsMaestro;
+    public IRolVsMaestro RolesVsMaestro{
+        get
+        {
+            if(_rolVsMaestro == null)
+            {
+                _rolVsMaestro = new RolVsMaestroRepository(_context);
+            }
+            return _rolVsMaestro;
+        }
+    }
+    private SubmoduloRepository _submodulo;
+    public ISubmodulo Submodulos
+    {
+        get
+        {
+            if(_submodulo == null)
+            {
+                _submodulo = new SubmoduloRepository(_context);
+            }
+            return _submodulo;
+        }
+    }
+    private TipoNotificacionRepository _tipoNotificacion;
+    public ITipoNotificacion TipoNotificaciones
+    {
+        get
+        {
+            if(_tipoNotificacion == null)
+            {
+                _tipoNotificacion = new TipoNotificacionRepository(_context);
+            }
+            return _tipoNotificacion;
+        }
+    }
+    private TipoRequerimientoRepository _tipoRequerimiento;
+    public ITipoRequerimiento TipoRequerimientos
+    {
+        get
+        {
+            if(_tipoRequerimiento == null)
+            {
+                _tipoRequerimiento = new TipoRequerimientoRepository(_context); 
+            }
+            return _tipoRequerimiento;
+        }
+    }
     public void Dispose()
     {
         throw new NotImplementedException();
+    }
+    public async Task<int> SaveAsync()
+    {
+        return await _context.SaveChangesAsync();
     }
 }
